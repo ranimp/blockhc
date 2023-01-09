472b204a82acce54da58fb9487d4049baf79b067
Rani Meliyana Putri <ranimeliyanaputri@gmail.com>
2023-01-09 16:53:09 +0800

    dashboard admin - manajemen konsultasi dn

---
+++ components/dashboard-admin/manajemen-hasil-konsultasi/adm-tambah-konsultasi.tsx
@@ -0,0 +1,88 @@
+import { useRouter } from 'next/router';
+import Button from '../../button/index';
+
+const TambahKonsultasiAdmin = ({
+  name, doctor, date, keluhan, diagnosa, tekanan, gula,
+}) => {
+  const router = useRouter();
+  return (
+    <div className="w-full">
+      <h3 className="font-bold text-sm md:text-xl my-2 md:my-4">Tambah Hasil Konsultasi</h3>
+      <table className="w-full text-black border-separate border-spacing-y-2">
+        <tbody>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Nama Pasien</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <input type="text" value={name} placeholder="Nama Pasien" className="focus:outline-none bg-transparent w-full" />
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Nama Dokter</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base capitalize">
+              <input type="text" value={doctor} placeholder="Nama Dokter" className="focus:outline-none bg-transparent w-full" />
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Tanggal</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <input type="text" value={date} placeholder="01/01/2022" className="focus:outline-none bg-transparent w-full" />
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Kategori</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base pr-4">
+              <select className="focus:outline-none bg-transparent w-full">
+                <option>Umum</option>
+              </select>
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Keluhan</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <input type="text" value={keluhan} placeholder="Keluhan" className="focus:outline-none bg-transparent w-full" />
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">Hasil Diagnosa</td>
+            <td className="text-xs sm:text-sm lg:text-base">:</td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <input type="text" value={diagnosa} placeholder="Diagnosa" className="focus:outline-none bg-transparent w-full" />
+            </td>
+          </tr>
+          <tr className="odd:bg-odd-blue even:bg-even-blue">
+            <td className="py-3 pl-3 text-xs lg:text-base">
+              <p>Lainnya</p>
+              <p>Tekanan Darah</p>
+              <p>Gula Darah Sewaktu</p>
+            </td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <p>:</p>
+              <p>:</p>
+              <p>:</p>
+            </td>
+            <td className="text-xs sm:text-sm lg:text-base">
+              <p className="invisible">nothing</p>
+              <p><input type="text" value={tekanan} placeholder="Tekanan darah" className="focus:outline-none bg-transparent w-full" /></p>
+              <p><input type="text" value={gula} placeholder="Gula darah" className="focus:outline-none bg-transparent w-full" /></p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="flex justify-end mt-2 gap-2">
+        <div className="w-24 lg:w-48">
+          <Button type="btn-outline" title="kembali" onClick={() => router.back()} />
+        </div>
+        <div className="w-24 lg:w-48">
+          <Button type="btn-normal" title="simpan" onClick={() => router.back()} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TambahKonsultasiAdmin;
---
+++ pages/dashboard-admin/manajemen-konsultasi/update-konsultasi.tsx
@@ -0,0 +1,52 @@
+import { Fragment, useState } from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import NavbarLogin from '../../../components/navbar/login';
+import Profil from '../../../components/dashboard/profil';
+import Sidebar from '../../../components/dashboard/sidebar';
+import DaftarDokterAdmin from '../../../components/dashboard-admin/manajemen-dokter/adm-daftar-dokter';
+import Footer from '../../../components/footer/index';
+import DaftarPasienAdmin from '../../../components/dashboard-admin/manajemen-pasien/adm-daftar-pasien';
+import UpdateKonsultasiAdmin from '../../../components/dashboard-admin/manajemen-hasil-konsultasi/adm-update-konsultasi';
+
+export default function RiwayatKonsultasiAdminPage() {
+  const [active, setActive] = useState('hasil-konsultasi');
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <title>Blockchain Health Care</title>
+      </Head>
+      <nav>
+        <NavbarLogin dashboard />
+      </nav>
+      <main className="px-4 lg:px-16 mt-28 flex flex-col min-h-screen justify-between">
+        <div className="flex justify-start">
+          <div className="w-1/9 md:w-1/3">
+            <div className="hidden md:block">
+              <Profil name="Rani Meliyana Putri" role="admin" />
+            </div>
+            <Sidebar menu3Show onClickMenu1={() => setActive('manajemen-pasien')} menu1={active === 'manajemen-pasien' && true} title1="manajemen pasien" onClickMenu2={() => router.push('/dashboard-admin')} menu2={active === 'hasil-konsultasi' && true} title2="manajemen hasil konsultasi" onClickMenu3={() => setActive('manajemen-dokter')} menu3={active === 'manajemen-dokter' && true} title3="manajemen dokter" />
+          </div>
+          <div className="w-full pl-6 sm:pl-8 lg::pl-12 flex flex-col gap-4">
+            <div className="flex gap-4 border-2 border-slate-300 w-full p-2 md:p-3 text-xs md:text-base rounded-md">
+              <figure>
+                <Image src="/icons/search.svg" alt="search" width={20} height={20} />
+              </figure>
+              <input type="text" placeholder="Pencarian" className="w-full focus:outline-none" />
+            </div>
+            <div>
+              {active === 'manajemen-pasien' && <DaftarPasienAdmin />}
+              {active === 'hasil-konsultasi' && <UpdateKonsultasiAdmin />}
+              {active === 'manajemen-dokter' && <DaftarDokterAdmin />}
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+}
